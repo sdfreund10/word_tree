f23d07514d75a1e67f84f13c5e5ba0c083ceb470
Stephen Freund <sdfreund10@gmail.com>
2019-02-04 22:03:21 -0500

    Lint setup file

--- test/test.rb
+++ test/test.rb
@@ -1,6 +1,7 @@
+# frozen_string_literal: true
+
 require_relative './word_tree/english_words_test'
 require_relative './word_tree/word_test'
 require_relative './word_tree/path_finder_test'
 require_relative './fast_word_tree/connections_test'
 require_relative './fast_word_tree/path_finder_test'
-
