880ac598d47f18efa5accc3534146def4cb3ce73
Stephen Freund <sdfreund10@gmail.com>
2019-02-04 21:59:22 -0500

    Add Rakefile Gemfile and test

--- test.rb
+++
@@ -1,7 +0,0 @@
-require_relative './test/word_tree/english_words_test'
-require_relative './test/word_tree/word_test'
-require_relative './test/word_tree/path_finder_test'
-require_relative './test/util/connection_computer_test'
-require_relative './test/fast_word_tree/connections_test'
-require_relative './test/fast_word_tree/path_finder_test'
-
---
+++ test/test.rb
@@ -0,0 +1,7 @@
+require_relative './word_tree/english_words_test'
+require_relative './word_tree/word_test'
+require_relative './word_tree/path_finder_test'
+require_relative './util/connection_computer_test'
+require_relative './fast_word_tree/connections_test'
+require_relative './fast_word_tree/path_finder_test'
+
